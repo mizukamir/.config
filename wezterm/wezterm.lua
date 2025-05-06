cfe41352dd7fb91e29391fd6c94f04ede9814bc0
mizukamir <nexus.on.water.r@gmail.com>
2025-05-06 22:16:13 +0900

    add macOS setting

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -4,4 +4,8 @@ local config = wezterm.config_builder()
 config.automatically_reload_config = true
 config.window_background_opacity = 0.85
 
+if wezterm.target_triple == "x86_64-apple-darwin" then
+	config.default_prog = { "/usr/local/bin/fish", "-l" }
+end
+
 return config
