dc6337587b598b8c824e0b9d5b81e4c69097d211
mizumine <13017007+mizumine@users.noreply.github.com>
2026-01-10 20:42:44 +0900

    Update wezterm config via home-manager

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,11 +1,1 @@
-local wezterm = require("wezterm")
-local config = wezterm.config_builder()
-
-config.automatically_reload_config = true
-config.window_background_opacity = 0.85
-
-if wezterm.target_triple == "x86_64-apple-darwin" then
-	config.default_prog = { "/usr/local/bin/fish", "-l" }
-end
-
-return config
+/nix/store/a5ms9v7nbqgs8pni715sxh6b7pffx83q-home-manager-files/.config/wezterm/wezterm.lua
